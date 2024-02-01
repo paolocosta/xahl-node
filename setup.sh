854c05cdfef8812724aba0e7a523d961680c1b7f
go140point6 <ryan@go140point6.com>
2024-02-01 11:27:49 -0800

    missing sudo for sed command

--- setup.sh
+++ setup.sh
@@ -68,7 +68,7 @@ FUNC_CLONE_NODE_SETUP(){
     sudo ./xahaud-install-update.sh
     sleep 2s
     echo -e "${YELLOW}Updating conf file to limit public RPC/WS to localhost ...${NC}"
-    sed -i -E '/^\[port_(ws|rpc)_public\]$/,/^\[/ {/^(ip = )0\.0\.0\.0/s/^(ip = )0\.0\.0\.0/\1127.0.0.1/}' /opt/xahaud/etc/xahaud.cfg
+    sudo sed -i -E '/^\[port_(ws|rpc)_public\]$/,/^\[/ {/^(ip = )0\.0\.0\.0/s/^(ip = )0\.0\.0\.0/\1127.0.0.1/}' /opt/xahaud/etc/xahaud.cfg
     sleep 2s
     sudo systemctl restart xahaud.service
     sleep 2s
